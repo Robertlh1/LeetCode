3414e3660b14ac7e4dba2987db4ca7abe18976b0
Robert Hasel <rlhasel1@gmail.com>
2022-09-24 20:30:16 -0700

    adding stats for previous problem

--- 1480. Running Sum of 1d Array/runningSum.js
+++ 1480. Running Sum of 1d Array/runningSum.js
@@ -1,3 +1,10 @@
+/*
+Note: Runtime is highly variable between submissions and I don't have a premium subscription,
+  which places me in a lower tier.
+Runtime: 89 ms, faster than 62.08% of JavaScript online submissions for Running Sum of 1d Array.
+Memory Usage: 42.4 MB, less than 48.60% of JavaScript online submissions for Running Sum of 1d Array.
+*/
+
 var runningSum = function(nums) {
   let result = []
 
