16f249b3368c787bfc6bcd96f31c7bdbb0ed05ee
Robert Hasel <rlhasel1@gmail.com>
2022-09-25 20:44:35 -0700

    Pushing tests for climbStairs

--- 70. Climbing Stairs/climbStairs.test.js
+++ 70. Climbing Stairs/climbStairs.test.js
@@ -4,7 +4,10 @@ describe("Testing climbStairs", () => {
   test("climbStairs() should return 2 when the input is 2", () => {
     expect(climbStairs(2)).toBe(2);
   })
-  test("climbStairs() should return 3 when the input is [1,3,5,6], 2", () => {
+  test("climbStairs() should return 3 when the input is 3", () => {
     expect(climbStairs(3)).toBe(3);
+  })
+  test("climbStairs() should return 8 when the input is 5", () => {
+    expect(climbStairs(5)).toBe(8);
   })
 })
