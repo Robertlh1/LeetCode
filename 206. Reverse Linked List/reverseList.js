16bc4fd9df13be5e23c584dafda1d7bb1cf96a89
Robert Hasel <rlhasel1@gmail.com>
2022-09-29 15:44:53 -0700

    Completed 206. Reverse Linked List

---
+++ 206. Reverse Linked List/reverseList.js
@@ -0,0 +1,33 @@
+/*
+Note: Runtime is highly variable between submissions and I don't have a premium subscription,
+  which places me in a lower tier.
+Runtime: 61 ms, faster than 98.44% of JavaScript online submissions for Reverse Linked List.
+Memory Usage: 44.1 MB, less than 71.54% of JavaScript online submissions for Reverse Linked List.
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+*/
+
+var reverseList = function(head) {
+  if (head === null || head.next === null) {
+      return head
+  }
+
+  let rev = new ListNode()
+  let temp = []
+  let cur = rev
+
+  while (head !== null) {
+      temp.push(head.val)
+      head = head.next
+  }
+
+  for (let i = temp.length - 1; i >= 0; i--) {
+      cur.val = temp[i]
+      if (i !== 0) {
+          cur.next = new ListNode()
+          cur = cur.next
+      }
+  }
+
+  return rev
+};
