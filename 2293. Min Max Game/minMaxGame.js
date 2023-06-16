87704aec3528e17fd3926124341df3dc8dea0b65
Robert Hasel <rlhasel1@gmail.com>
2023-06-15 21:46:12 -0700

    Missing prompt description

--- 2293. Min Max Game/minMaxGame.js
+++ 2293. Min Max Game/minMaxGame.js
@@ -4,8 +4,16 @@ Note: Runtime is highly variable between submissions and I don't have a premium 
 Runtime 54 ms Beats 94.29%
 Memory 42.6 MB Beats 88.57%
 
-Given an integer array nums sorted in non-decreasing order, return an array of the
-squares of each number sorted in non-decreasing order.
+You are given a 0-indexed integer array nums whose length is a power of 2.
+
+Apply the following algorithm on nums:
+
+Let n be the length of nums. If n == 1, end the process. Otherwise, create a new 0-indexed integer array newNums of length n / 2.
+For every even index i where 0 <= i < n / 2, assign the value of newNums[i] as min(nums[2 * i], nums[2 * i + 1]).
+For every odd index i where 0 <= i < n / 2, assign the value of newNums[i] as max(nums[2 * i], nums[2 * i + 1]).
+Replace the array nums with newNums.
+Repeat the entire process starting from step 1.
+Return the last number that remains in nums after applying the algorithm.
 */
 
 var minMaxGame = function(nums) {
